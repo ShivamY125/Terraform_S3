d979b418fca21505f8ca060288e9bf5368d1539d
Shivam Yadav <yshivam91yadav@gmail.com>
2026-01-18 18:31:50 +0530

    KMS key encyption & S3 bucket policy addition

--- main.tf
+++ main.tf
@@ -28,3 +28,71 @@ resource "aws_s3_object" "test_upload_bucket" {
   }
 }
 
+# Step 3: Encyption and Enable KMS Key SSE-KMS.
+
+resource "aws_kms_key" "S3_bucket_kms_key" {
+  description = "KMS key for S3 Bucket"
+  deletion_window_in_days = 7
+  tags = {
+    name = "KMS key for S3 Bucket"
+  }
+  
+}
+
+resource "aws_kms_alias" "S3_bucket_kms_key_alias" {
+  name = "alias/s3_bucket_kms_key_alias"
+  target_key_id = aws_kms_key.S3_bucket_kms_key.id
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "s3_bucket_encyption_with_kms_key" {
+  bucket = aws_s3_bucket.test_bucket.id # picking this bucket id from step 1.
+
+  rule{
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.S3_bucket_kms_key.arn
+      sse_algorithm = "aws:kms"
+    }
+  }
+  
+}
+
+# Step 4: Working with IAM Policy for S3 Bucket.
+# Allow get object permission to everyone.action.
+
+resource "aws_s3_bucket_public_access_block" "enable_public_access" {
+  bucket = aws_s3_bucket.test_bucket.id
+
+  block_public_acls = false
+  block_public_policy = false
+  ignore_public_acls = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "test_bucket_read_policy" {
+  bucket = aws_s3_bucket.test_bucket.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = "*"
+        Action = [
+          "s3:GetObject"
+        ]
+        Resource = "${aws_s3_bucket.test_bucket.arn}/*"
+      }
+    ]
+  })
+
+  depends_on = [ aws_s3_bucket_public_access_block.enable_public_access ]
+}
+
+# Step 5 Versioning.
+
+resource "aws_s3_bucket_versioning" "test_bucket_versioning" {
+  bucket = aws_s3_bucket.test_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+  
+}
